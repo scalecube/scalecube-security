6d9338e6c605afb026694b31a2770b83a27b616b
aharonha <aharon@om2.com>
2018-12-23 12:12:56 +0200

    document `AuthenticationException`

--- JWT/src/main/java/io/scalecube/security/AuthenticationException.java
+++ JWT/src/main/java/io/scalecube/security/AuthenticationException.java
@@ -1,10 +1,13 @@
 package io.scalecube.security;
 
+/**
+ * This class encapsulates all authentication exceptions. This will allow us to replace the internal
+ * mechanism of authentication without changing client code.
+ */
+
 public class AuthenticationException extends RuntimeException {
 
-  /**
-   *  Generated serial Version UID.
-   */
+  /** Generated serial Version UID. */
   private static final long serialVersionUID = 3524768727973503394L;
 
   public AuthenticationException(String message, Throwable cause) {
