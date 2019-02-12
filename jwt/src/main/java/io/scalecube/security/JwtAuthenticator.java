505342ea717c56db9582d32ea1632da303aa17cd
aharonha <aharon@om2.com>
2019-02-12 13:14:43 +0200

    default method: profileFromClaims

--- jwt/src/main/java/io/scalecube/security/JwtAuthenticator.java
+++ jwt/src/main/java/io/scalecube/security/JwtAuthenticator.java
@@ -1,5 +1,6 @@
 package io.scalecube.security;
 
+import io.jsonwebtoken.Claims;
 import io.scalecube.security.auth.Authenticator;
 import reactor.core.publisher.Mono;
 
@@ -12,4 +13,17 @@ public interface JwtAuthenticator extends Authenticator {
    * @return security profile.
    */
   Mono<Profile> authenticate(String token);
+  
+  default Profile profileFromClaims(Claims tokenClaims) {
+    return Profile.builder()
+        .userId(tokenClaims.get("sub", String.class))
+        .tenant(tokenClaims.get("aud", String.class))
+        .email(tokenClaims.get("email", String.class))
+        .emailVerified(tokenClaims.get("email_verified", Boolean.class))
+        .name(tokenClaims.get("name", String.class))
+        .familyName(tokenClaims.get("family_name", String.class))
+        .givenName(tokenClaims.get("given_name", String.class))
+        .claims(tokenClaims)
+        .build();
+  }
 }
