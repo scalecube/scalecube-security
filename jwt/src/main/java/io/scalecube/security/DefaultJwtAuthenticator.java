bdc29a6c4470d3b30dd24791037bb728d1e63f95
aharonha <aharon@om2.com>
2019-02-12 13:15:58 +0200

    switch from Mono.create to Mono.just, reuse code and readability

--- jwt/src/main/java/io/scalecube/security/DefaultJwtAuthenticator.java
+++ jwt/src/main/java/io/scalecube/security/DefaultJwtAuthenticator.java
@@ -1,13 +1,9 @@
 package io.scalecube.security;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.UnsupportedJwtException;
-import io.jsonwebtoken.security.SignatureException;
 import reactor.core.publisher.Mono;
 
 public final class DefaultJwtAuthenticator implements JwtAuthenticator {
@@ -20,32 +16,9 @@ public final class DefaultJwtAuthenticator implements JwtAuthenticator {
 
   @Override
   public Mono<Profile> authenticate(String token) {
-    return Mono.create(
-        sink -> {
-
-          try {
-            Jws<Claims> claims = jwtParser.parseClaimsJws(token);
-            Claims tokenClaims = claims.getBody();
-
-            sink.success(
-                Profile.builder()
-                    .userId(tokenClaims.get("sub", String.class))
-                    .tenant(tokenClaims.get("aud", String.class))
-                    .email(tokenClaims.get("email", String.class))
-                    .emailVerified(tokenClaims.get("email_verified", Boolean.class))
-                    .name(tokenClaims.get("name", String.class))
-                    .familyName(tokenClaims.get("family_name", String.class))
-                    .givenName(tokenClaims.get("given_name", String.class))
-                    .claims(tokenClaims)
-                    .build());
-          } catch (ExpiredJwtException
-              | UnsupportedJwtException
-              | MalformedJwtException
-              | SignatureException
-              | IllegalArgumentException exception) {
-            sink.error(exception);
-            return;
-          }
-        });
+    return Mono.just(token)
+        .map(unparsedToken -> jwtParser.parseClaimsJws(unparsedToken))
+        .map(Jws<Claims>::getBody)
+        .map(this::profileFromClaims);
   }
 }
