ceb51ee9bf819888fa4b24a410fc84449d687c5b
ronen <ronen.hamias@gmail.com>
2019-02-15 07:58:31 +0200

    rename

--- jwt/src/main/java/io/scalecube/security/auth/Authenticator.java
+++ jwt/src/main/java/io/scalecube/security/auth/Authenticator.java
@@ -8,8 +8,8 @@ public interface Authenticator {
   /**
    * Authenticate the identity of one.
    *
-   * @param who a string of identity, can be a token, a user & password etc,.
+   * @param token a string of identity, can be a token, a user & password etc,.
    * @return a mono with profile identifying the one, or mono with an error.
    */
-  Mono<Profile> authenticate(String who);
+  Mono<Profile> authenticate(String token);
 }
