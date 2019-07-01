dbccb74d929704130cfc13a957bccff93c61ce99
Dmytro Lazebnyi <d.lazebny@gmail.com>
2019-07-01 12:33:01 +0300

    Use reactive approach

--- jwt/src/test/java/io/scalecube/security/acl/AccessControlTest.java
+++ jwt/src/test/java/io/scalecube/security/acl/AccessControlTest.java
@@ -1,6 +1,7 @@
 package io.scalecube.security.acl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import io.jsonwebtoken.Jwts;
 import io.scalecube.security.api.Authenticator;
 import io.scalecube.security.jwt.DefaultJwtAuthenticator;
@@ -10,30 +11,32 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
-public class AccessControlTest {
+class AccessControlTest {
 
-  // user permissions 
+  // user permissions
   private static final String RESOURCE_READ = "resource/read";
   private static final String RESOURCE_CREATE = "resource/create";
   private static final String RESOURCE_DELETE = "resource/delete";
-  
+
   // user roles
   private static final String OWNER = "owner";
   private static final String ADMIN = "admin";
   private static final String MEMBER = "member";
-  
+
   private static SecretKey key;
-  private static DefaultAccessControl accessContorl;
+  private static DefaultAccessControl accessControl;
 
   @BeforeAll
-  public static void setUp() throws Exception {
+  static void setUp() throws Exception {
     key = KeyGenerator.getInstance("HmacSHA256").generateKey();
-    Authenticator authenticator =
-        new DefaultJwtAuthenticator(m -> "1".equals(m.get("kid")) ? key : null);
 
-    accessContorl =
+    Authenticator authenticator =
+        new DefaultJwtAuthenticator(m -> "1".equals(m.get("kid")) ? Mono.just(key) : Mono.empty());
+
+    accessControl =
         DefaultAccessControl.builder()
             .authenticator(authenticator)
             .authorizer(
@@ -46,16 +49,12 @@ public class AccessControlTest {
   }
 
   @Test
-  public void shouldGrantAccess() throws NoSuchAlgorithmException {
+  void shouldGrantAccess() throws NoSuchAlgorithmException {
 
     String token =
-        Jwts.builder()
-            .setHeaderParam("kid", "1")
-            .claim("roles", OWNER)
-            .signWith(key)
-            .compact();
+        Jwts.builder().setHeaderParam("kid", "1").claim("roles", OWNER).signWith(key).compact();
 
-    StepVerifier.create(accessContorl.check(token, RESOURCE_CREATE))
+    StepVerifier.create(accessControl.check(token, RESOURCE_CREATE))
         .assertNext(
             profile -> {
               assertEquals(profile.claim("roles"), OWNER);
@@ -64,16 +63,12 @@ public class AccessControlTest {
   }
 
   @Test
-  public void shouldDenyAccess() throws NoSuchAlgorithmException {
+  void shouldDenyAccess() throws NoSuchAlgorithmException {
 
     String token =
-        Jwts.builder()
-            .setHeaderParam("kid", "1")
-            .claim("roles", MEMBER)
-            .signWith(key)
-            .compact();
+        Jwts.builder().setHeaderParam("kid", "1").claim("roles", MEMBER).signWith(key).compact();
 
-    StepVerifier.create(accessContorl.check(token, RESOURCE_DELETE))
+    StepVerifier.create(accessControl.check(token, RESOURCE_DELETE))
         .expectError(AccessControlException.class)
         .verify();
   }
