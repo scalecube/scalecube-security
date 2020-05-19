368732a7a915b142cfbe393e89fc35cc6bfe59a3
Artem Vysochyn <artem.vysochyn@gmail.com>
2020-05-19 14:36:41 +0300

    Fixed javadoc

--- jwt/src/main/java/io/scalecube/security/api/Authenticator.java
+++ jwt/src/main/java/io/scalecube/security/api/Authenticator.java
@@ -7,7 +7,7 @@ public interface Authenticator {
   /**
    * Authenticate the identity of one.
    *
-   * @param token a string of identity, can be a token, a user & password etc,.
+   * @param token a string of identity, can be a token, a user and password etc,.
    * @return a mono with profile identifying the one, or mono with an error.
    */
   Mono<Profile> authenticate(String token);
