6e90b388e2dece0aa52c958a9d66e72d6683a301
segabriel <sergiy.gabriel@jivygroup.com>
2022-04-06 18:14:10 +0300

    Add ability to parse body without verification

--- tokens/src/main/java/io/scalecube/security/tokens/jwt/JwtTokenResolver.java
+++ tokens/src/main/java/io/scalecube/security/tokens/jwt/JwtTokenResolver.java
@@ -3,8 +3,15 @@ package io.scalecube.security.tokens.jwt;
 import java.util.Map;
 import reactor.core.publisher.Mono;
 
-@FunctionalInterface
 public interface JwtTokenResolver {
+
+  /**
+   * Parses and returns token claims without verification.
+   *
+   * @param token jwt token
+   * @return parsed claims
+   */
+  Map<String, Object> parseBody(String token);
 
   /**
    * Verifies and returns token claims if everything went ok.
