ce67997197c5015e768c8eb774a2518a685aa536
Artem Vysochyn <artem.vysochyn@gmail.com>
2025-02-02 19:56:38 +0200

    Renamed test

--- tests/src/test/java/io/scalecube/security/tokens/jwt/JsonwebtokenResolverTests.java
+++ tests/src/test/java/io/scalecube/security/tokens/jwt/JsonwebtokenResolverTests.java
@@ -18,7 +18,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class VaultIdentityTokenTests {
+public class JsonwebtokenResolverTests {
 
   private static VaultEnvironment vaultEnvironment;
 
